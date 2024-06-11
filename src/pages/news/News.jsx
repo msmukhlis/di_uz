b518a93d19c4d3efd05007618647fa1980c80c78
msmukhlis <msmukhlisss@gmail.com>
2024-06-11 16:43:59 +0500

    added

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -7,7 +7,7 @@ import news4 from "../../../public/images/news4.webp"
 import { useTranslation } from 'react-i18next'
 
 export const News = () => {
-           
+
   const { t, i18n } = useTranslation();
 
   const handleChange = (selectedLanguage) => {
@@ -22,28 +22,28 @@ export const News = () => {
         <h2>{t("News")}</h2>
         <p>{t("We don't just create projects, we create new opportunities. By building state-scale facilities, we are building the future. We care about the environment:")}</p>
       </div>
-    <div className={styles.boxes}>
-       <div data-aos="fade-up" className={styles.box}>
-                <img src={news1} alt="photo" className={styles.img}/>
-                <p className={styles.date}><span></span>2023-01-27</p>
-                <p className={styles.text}>{t("Who is the project manager of construcktion?")}</p>
-              </div>
-              <div data-aos="fade-up" className={styles.box}>
-                <img src={news2} alt="photo" className={styles.img}/>
-                <p className={styles.date}><span></span>2022-12-31</p>
-                <p className={styles.text}>{t("Discover Invest congratulates everyone on a coming Happy New Year 2023!")}</p>
-              </div>
-              <div data-aos="fade-up" className={styles.box}>
-                <img src={news3} alt="photo" className={styles.img}/>
-                <p className={styles.date}><span></span>2022-10-02</p>
-                <p className={styles.text}>{t("THE DAY WHEN THE CONSTITUTION OF THE REPUBLIC OF UZBEKISTAN WAS ADOPTED")}</p>
-              </div>
-              <div data-aos="fade-up" className={styles.box}>
-                <img src={news4} alt="photo" className={styles.img}/>
-                <p className={styles.date}><span></span>2022-12-19</p>
-                <p className={styles.text}>{t("We continue to introduce you to Discover Invest's wide range of construction properties.")}</p>
-              </div>
-    </div>
+      <div className={styles.boxes}>
+        <div data-aos="fade-up" className={styles.box}>
+          <img src={news1} alt="photo" className={styles.img} />
+          <p className={styles.date}><span></span>2023-01-27</p>
+          <p className={styles.text}>{t("Who is the project manager of construcktion?")}</p>
+        </div>
+        <div data-aos="fade-up" className={styles.box}>
+          <img src={news2} alt="photo" className={styles.img} />
+          <p className={styles.date}><span></span>2022-12-31</p>
+          <p className={styles.text}>{t("Discover Invest congratulates everyone on a coming Happy New Year 2023!")}</p>
+        </div>
+        <div data-aos="fade-up" className={styles.box}>
+          <img src={news3} alt="photo" className={styles.img} />
+          <p className={styles.date}><span></span>2022-10-02</p>
+          <p className={styles.text}>{t("THE DAY WHEN THE CONSTITUTION OF THE REPUBLIC OF UZBEKISTAN WAS ADOPTED")}</p>
+        </div>
+        <div data-aos="fade-up" className={styles.box}>
+          <img src={news4} alt="photo" className={styles.img} />
+          <p className={styles.date}><span></span>2022-12-19</p>
+          <p className={styles.text}>{t("We continue to introduce you to Discover Invest's wide range of construction properties.")}</p>
+        </div>
+      </div>
     </div>
   )
 }
